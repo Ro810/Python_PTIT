d73499e25e0b570695dfbef2721456ad29d22cbd
lphuong08 <PhuongTDL.B23CN667@stu.ptit.edu.vn>
2025-08-15 11:28:13 +0700

    Create PY01036-TÍNH TỔNG PHÂN THỨC.py

---
+++ PY01036-TÍNH TỔNG PHÂN THỨC.py
@@ -0,0 +1,16 @@
+def gcd(a, b):
+    while b: 
+        a, b = b, a % b
+        return a
+    
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    sum = 0
+    if n % 2 == 0:
+        for i in range(2, n + 1, 2):
+            sum += (1 / i)
+    else:
+        for i in range(1, n + 1, 2):
+            sum += (1 / i)
+    print(f"{sum:.6f}")
