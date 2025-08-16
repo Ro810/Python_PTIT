0b24ff752ce3883cb60086413d0be3a81d9b796c
lphuong08 <PhuongTDL.B23CN667@stu.ptit.edu.vn>
2025-08-16 20:39:41 +0700

    Create PY01053-SỐ CHIA HẾT CHO 3.py

---
+++ PY01053-SỐ CHIA HẾT CHO 3.py
@@ -0,0 +1,22 @@
+def nguyen_to(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def solve(n):
+    sum = 0
+    while n:
+        sum += n % 10
+        n //= 10
+    if sum % 3 == 0:
+        print("YES")
+    else:
+        print("NO")
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    solve(n)
